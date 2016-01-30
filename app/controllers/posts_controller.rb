ace9dac6941c4948a5e136094213333e141c09fc
David <luptak.david@gmail.com>
2016-01-30 12:07:07 +0100

    Video -- Post association.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,7 @@ class PostsController < ApplicationController
   # GET /posts/new
   def new
     @post = Post.new
+    @video = @post.build_video
   end
 
   # GET /posts/1/edit
@@ -71,6 +72,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :description) #.., :user_id)
+      params.require(:post).permit(:title, :description, video_attributes: [:link]) #.., :user_id)
     end
 end
