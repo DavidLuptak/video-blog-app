ac00031b79e95f4fef8e3f2636cb7e1ed2c1a282
David <luptak.david@gmail.com>
2016-01-31 17:54:24 +0100

    Authorization for listing users.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "the_video_blog_app@#{ENV['EMAIL_HOST']}"
+  config.mailer_sender = "the-video-blog-app@#{ENV['EMAIL_HOST']}"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
