989c29c83917487b58d40587bad10c9d59a6727d
David <luptak.david@gmail.com>
2016-02-07 11:21:12 +0100

    Category association correction.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
-  has_many :videos
+  has_and_belongs_to_many :videos
 
   def to_param
     name
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,15 +1,14 @@
 class Video < ActiveRecord::Base
   belongs_to :post
-  belongs_to :category
+  has_and_belongs_to_many :category
 
   YT_LINK_FORMAT = %r{\A.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*}i
 
   validates :link, presence: true, format: YT_LINK_FORMAT
 
-  after_save :checkCategory
+  after_save :check_category
 
-  def checkCategory
-     category = Category.find_or_create_by!(name: category_title)
+  def check_category
+    category << Category.find_or_create_by!(name: category_title)
   end
-
 end
