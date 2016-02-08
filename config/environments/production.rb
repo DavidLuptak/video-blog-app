ec89ef5220918db30d7641701f35d9601db4aa8b
David <luptak.david@gmail.com>
2016-02-08 22:41:08 +0100

    Exception notification -- action mailer configuration (raise delivery errors).

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,5 +95,5 @@ Rails.application.configure do
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 end
